7351a9c52fa9443e2f938d8d02f6bd49511e288f
余力 <[li.yu@dianrong.com]>
2018-01-17 15:34:03 +0800

    init project

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+const config = {
+	entry: './src/index.ts',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'eoe.bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.ts$/,
+				loader: 'ts-loader'
+			}
+		]
+	}
+};
+
+module.exports = config;
