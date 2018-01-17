9578ed46f2666f4bbc175d26277ecd7221ecf9c8
miyt <yunting.mi@dianrong.com>
2018-01-17 16:04:44 +0800

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
+'use strict';
+
 const path = require('path');
 
 const config = {
-	entry: './src/index.ts',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'eoe.bundle.js'
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.ts$/,
-				loader: 'ts-loader'
-			}
-		]
-	}
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'eoe.bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
 };
 
 module.exports = config;
